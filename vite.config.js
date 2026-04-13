3ffe0c36462090e36da7a4bac4670e955650a3d9
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2026-04-13 17:15:52 +0000

    ci: add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { deckPlugin, tailwindPlugin } from '@deckio/deck-engine/vite'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/deckio-digital-security/' : '/',
   plugins: [
     react({
       include: [/\.[jt]sx?$/, /node_modules\/@deckio\/deck-engine\/.+\.jsx$/],
